5dba6071bd59089da7e7003a9efd48cc6bf9ab78
Konsta Purtsi <kovipu@utu.fi>
2017-12-18 16:55:35 +0200

    check for POST request validity

--- server.js
+++ server.js
@@ -17,9 +17,14 @@ app.get("/api/data", (req, res) => {
 /* update the weather data */
 app.post("/api/data", (req, res) => {
   console.log("Received new POST request");
-  req.body.dateTime = new Date();
-  data.push(req.body);
-  res.status(200).json(req.body);
+  if (req.body.temperature === undefined) {
+    console.log("Invalid request!")
+    res.status(400).json(req.body);
+  } else {
+    req.body.dateTime = new Date();
+    data.push(req.body);
+    res.status(200).json(req.body);
+  }
 });
 
 const port = process.env.PORT ? process.env.PORT : 8001;
