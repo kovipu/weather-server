42cfd191c8bca0ad1181fa956ad1ecd8cef34b34
Konsta Purtsi <kovipu@utu.fi>
2018-03-11 16:42:21 +0200

    added dot to temp values

--- src/Temperature/Temperature.js
+++ src/Temperature/Temperature.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const temperature = ({ temperature }) => {
+    let temp;
+    if (temperature === undefined) {
+        temp = "No connection to server"
+    } else {
+        const pos = temperature.length - 2;
+        temp = temperature.slice(0, pos) + '.' + temperature.slice(pos);
+    }
     return (
         <div className="Temperatures">
-            <h1>Temperature is: {temperature}°C</h1>
+            <h1>Temperature is: {temp}°C</h1>
         </div>
     );
 }
